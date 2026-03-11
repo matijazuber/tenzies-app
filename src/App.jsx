65d71c15f8fd1ddf1c3730c821c59f87611776e3
Matija Zuber <matijazuber273@gmail.com>
2026-03-11 14:29:56 +0100

    created a state function that when clicking roll it generates the 10 new dice components with different values

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import Dice from "./components/dice";
+import { useState } from "react";
+
+function generateAllNewDice() {
+  let newDice = [];
+  for (let i = 0; i < 10; i++) {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    newDice.push(randomNumber);
+  }
+  return newDice;
+}
 
 export default function App() {
+  const [diceComponent, setDiceComponent] = useState(generateAllNewDice());
+
+  function rollDice() {
+    setDiceComponent(generateAllNewDice());
+  }
+
+  const diceElements = diceComponent.map((dice, index) => {
+    return <Dice key={index} value={dice}></Dice>;
+  });
   return (
     <>
       <main>
@@ -9,20 +28,11 @@ export default function App() {
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </div>
-        <div className="dice">
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-          <Dice value={1}></Dice>
-        </div>
+        <div className="dice-container">{diceElements}</div>
         <div>
-          <button className="roll">Roll</button>
+          <button onClick={rollDice} className="roll">
+            Roll
+          </button>
         </div>
       </main>
     </>
